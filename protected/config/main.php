fa1edd5d31519b60686b0fe5abe538868e9bd9f9
TraianAlex <victor_traian@yahoo.com>
2014-09-12 16:27:18 -0400

    prtected/config/main setupp index

--- protected/config/main.php
+++ protected/config/main.php
@@ -5,9 +5,19 @@
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
+function _joinpath($dir1, $dir2){
+    return realpath($dir1 . '/' . $dir2);
+}
+$homePath = dirname(__FILE__) . '\..\..';
+$protectedPath = _joinpath($homePath, 'protected');
+$webrootPath = _joinpath($homePath, 'www');
+$runtimePath = _joinpath($homePath, 'runtime');
+
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	//'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+        'basePath' => $protectedPath,
+        'runtimePath' => $runtimePath,
+	'name'=>'ayiitest',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -20,14 +30,14 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'gii',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
+
 	),
 
 	// application components
@@ -37,29 +47,32 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
+
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+                        //'showScriptName' => false,
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
+                /*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		// uncomment the following to use a MySQL database
-		/*
+		*/
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=ayiitest',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'aaaa',
 			'charset' => 'utf8',
+                        'enableParamLogging' => true,
+                        'enableProfiling' => true,
 		),
-		*/
+
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
@@ -69,14 +82,15 @@ return array(
 			'routes'=>array(
 				array(
 					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
+					'levels'=>'error, warning, trace',
 				),
 				// uncomment the following to show log messages on web pages
-				/*
+
 				array(
 					'class'=>'CWebLogRoute',
+                                        'levels'=>'error, warning, info',
 				),
-				*/
+
 			),
 		),
 	),
